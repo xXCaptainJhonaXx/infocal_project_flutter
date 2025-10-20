273fc4982ddb58003e62e2669b9a2ea142113490
xXCaptainJhonaXx <fabrizzioachabal070601@gmail.com>
2025-10-20 10:13:41 -0400

    20/10/25 - Widget AssetImage

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:infocalproject/review.dart';
 import 'description_place.dart';
 
 class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final descriptionPlace = Container(
+      margin: EdgeInsets.only(
+        top: 250,
+        left: 30,
+        right: 30,
+      ),
+      //parametros descriptionPlace
+      child: DescriptionPlace("Uyuni", 1, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris odio lectus, lacinia vel congue eu, fringilla id sem. Mauris faucibus diam purus, eget tincidunt arcu vestibulum vitae. Duis non orci vitae augue sodales eleifend consequat vel massa. Suspendisse potenti. Nam nec fermentum sem, eget sodales odio. Fusce sed volutpat massa. In vehicula ut tortor in consequat. Duis bibendum ipsum quis purus faucibus, et fringilla est pulvinar. Sed laoreet non orci non bibendum. Donec rhoncus eget est quis imperdiet. Vestibulum vitae aliquet magna, nec tempus eros. Ut interdum dolor ut elit hendrerit, id porta libero consequat. Nam vulputate elit quis aliquam mollis. Praesent ornare sagittis ornare."),
+    );
+
+    final review = Container(
+      margin: EdgeInsets.only(
+        top: 300,
+        left: 30,
+        right: 30,
+      ),
+      height: 80,
+      child: Review(
+          "assets/images/pexels-sulimansallehi-1704488.jpg",
+          "Maria Elena",
+          "1 reviews - 3 photos",
+          2,
+          "Buen lugar para visitar."),
+    );
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
@@ -14,15 +41,8 @@ class MyHome extends StatelessWidget {
           Container(
             color: Colors.white,
           ),
-          Container(
-            margin: EdgeInsets.only(
-              top: 250,
-              left: 30,
-              right: 30,
-            ),
-            //parametros descriptionPlace
-            child: DescriptionPlace("Uyuni", 1, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris odio lectus, lacinia vel congue eu, fringilla id sem. Mauris faucibus diam purus, eget tincidunt arcu vestibulum vitae. Duis non orci vitae augue sodales eleifend consequat vel massa. Suspendisse potenti. Nam nec fermentum sem, eget sodales odio. Fusce sed volutpat massa. In vehicula ut tortor in consequat. Duis bibendum ipsum quis purus faucibus, et fringilla est pulvinar. Sed laoreet non orci non bibendum. Donec rhoncus eget est quis imperdiet. Vestibulum vitae aliquet magna, nec tempus eros. Ut interdum dolor ut elit hendrerit, id porta libero consequat. Nam vulputate elit quis aliquam mollis. Praesent ornare sagittis ornare."),
-          ),
+          //descriptionPlace
+          review
         ],
       ),
     );
---
+++ lib/review.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+
+class Review extends StatelessWidget {
+  String pathPhoto;
+  String textoNombreUsuario;
+  String textoResumenUsuario;
+  int cantidadEstrella;
+  String textoComentario;
+
+  Review(this.pathPhoto, this.textoNombreUsuario, this.textoResumenUsuario, this.cantidadEstrella, this.textoComentario);
+
+
+  @override
+  Widget build(BuildContext context) {
+    //review
+    final foto = Container(
+      margin: EdgeInsets.only(
+        right: 10
+      ),
+      width: 80,
+      height: 80,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+            image: AssetImage(
+                pathPhoto
+            ),
+          fit: BoxFit.cover
+        ),
+      ),
+    );
+
+
+    //NOMBRE DE USUARIO
+    final nombreUsuario = Container(
+      child: Text(
+          textoNombreUsuario,
+          style: TextStyle(
+            fontFamily: "Lato",
+            fontSize: 20
+        ),
+      ),
+    );
+
+    //RESUMEN USUARIO
+    final resumenUsuario = Container(
+      margin: EdgeInsets.only(
+        right: 10,
+      ),
+      child: Text(
+          textoResumenUsuario,
+        style: TextStyle(
+          color: Colors.black54,
+          fontFamily: "Lato"
+        ),
+      ),
+    );
+
+    //fila estrella
+    final estrella = Container(
+      margin: EdgeInsets.only(
+          right: 5
+      ),
+      child: Icon(
+        Icons.star,
+        color: Colors.amber,
+        size: 18,
+      ),
+    );
+
+    final estrellaBorde = Container(
+      margin: EdgeInsets.only(
+          right: 5
+      ),
+      child: Icon(
+          Icons.star,
+          color: Colors.black54
+      ),
+    );
+
+    //fila estrella
+
+    List<Container> estrellas = [];
+    for (int i = 0; i < 5; i++) {
+      if (i < cantidadEstrella) {
+        estrellas.add(estrella);
+      } else {
+        estrellas.add(estrellaBorde);
+      }
+    }
+
+
+    final filaEstrellas = Row(
+      children: estrellas,
+    );
+
+    //FILA RESUMEN
+    final filaResumen = Row(
+      children: <Widget>[
+        resumenUsuario, filaEstrellas
+
+      ],
+    );
+
+    //COMENTARIO
+    
+    final comentario = Container(
+      child: Text(
+          textoComentario,
+          style: TextStyle(
+            fontFamily: "Lato"
+          ),
+      ),
+    );
+
+    //COLUMNA REVIEW
+    final columnaReview = Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        nombreUsuario,
+        filaResumen,
+        comentario
+      ],
+    );
+
+
+    final review = Row(
+      children: <Widget>[
+        foto, columnaReview
+      ],
+    );
+
+    return review;
+  }
+}
