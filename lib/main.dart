36cded89146cea96dfb1211036855a421f78a6d8
xXCaptainJhonaXx <fabrizzioachabal070601@gmail.com>
2025-11-10 15:27:12 -0400

    10/11/25 - CupertinoTabScaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:infocalproject/places.dart';
+import 'package:infocalproject/places_cupertino.dart';
 import 'home.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity
       ),
-      home: Places(),
+      home: PlacesCupertino(),
     );
   }
   
---
+++ lib/places_cupertino.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/cupertino.dart';
+import 'package:infocalproject/home.dart';
+import 'package:infocalproject/profile_places.dart';
+import 'package:infocalproject/search_places.dart';
+
+class PlacesCupertino extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    final places = Scaffold(
+      bottomNavigationBar: CupertinoTabScaffold(
+          tabBar: CupertinoTabBar(
+            backgroundColor: Colors.white.withAlpha(50),
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home,
+                    color: Color(0xFF574ACF),
+                  )
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.search,
+                    color: Color(0xFF574ACF),
+                  )
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.person,
+                    color: Color(0xFF574ACF),
+                  )
+              ),
+            ],
+          ),
+          tabBuilder: (BuildContext context, int index){
+            CupertinoTabView cupertinoTabView = new CupertinoTabView();
+
+            switch(index){
+              case 0:
+                cupertinoTabView = CupertinoTabView(
+                  builder: (BuildContext context) => MyHome(),
+                );
+                break;
+              case 1:
+                cupertinoTabView = CupertinoTabView(
+                  builder: (BuildContext context) => SearchPlaces(),
+                );
+                break;
+              case 2:
+                cupertinoTabView = CupertinoTabView(
+                  builder: (BuildContext context) => ProfilePlaces(),
+                );
+                break;
+            }
+
+            return cupertinoTabView;
+          }
+      ),
+    );
+
+    return places;
+  }
+}
